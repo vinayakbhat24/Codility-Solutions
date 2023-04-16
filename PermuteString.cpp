7de2c4ae822e7a6d0de4126b42431b75db032b2f
Vinayak Bhat <vinayak.tar@gmail.com>
2023-04-16 12:39:38 +0200

    Create PermuteString.cpp

---
+++ PermuteString.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+
+void permuteString(std::string str, std::string  ans)
+{
+    if(str.length() == 0 )
+    {
+        std::cout<<ans<<std::endl;
+    }
+    
+    for (auto  i = 0 ; i < str.size(); i++)
+    {
+        auto ch = str[i];
+        auto left =str.substr(0, i);
+        auto right = str.substr(i+1);
+        permuteString(left+right, ans+ch);
+        
+    }
+    
+}
+
+int main()
+{
+ permuteString("ABC", "");
+}
